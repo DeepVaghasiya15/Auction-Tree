2e1b61c0b40d9ff139e8972bbd5e28e823d230e2
Deep <deepvaghasiya15@gmail.com>
2024-07-19 11:03:34 +0530

    profile screen created

--- lib/model/property_model.dart
+++ lib/model/property_model.dart
@@ -1,31 +1,138 @@
-// class Property {
-//   final String title;
-//   final String address;
-//   final String description;
-//   final String type;
-//   final String builtUpArea;
-//   final String lotSize;
-//   final String imageAssetPath;
-//
-//   Property({
-//     required this.title,
-//     required this.address,
-//     required this.description,
-//     required this.type,
-//     required this.builtUpArea,
-//     required this.lotSize,
-//     required this.imageAssetPath,
-//   });
-//
-//   factory Property.fromJson(Map<String, dynamic> json) {
-//     return Property(
-//       title: json['title'],
-//       address: json['address'],
-//       description: json['description'],
-//       type: json['type'],
-//       builtUpArea: json['builtUpArea'],
-//       lotSize: json['lotSize'],
-//       imageAssetPath: json['imageAssetPath'],
-//     );
-//   }
-// }
+// property.dart
+
+class Property {
+  final String acceptBuyersTitle;
+  final String architextName;
+  final String assetType;
+  final String builderName;
+  final String builtupArea;
+  final String carParking;
+  final String interiorAccess;
+  final String ownership;
+  final String possesionTime;
+  final String propertyBuiltYear;
+  final String propertyTaxID;
+  final String rentable;
+  final String address;
+  final String auctionStatus;
+  final String builtUpArea;
+  final String category;
+  final String currentBid;
+  final String description;
+  final String id;
+  final String imageAssetPath;
+  final List<String> imagePaths;
+  final int index;
+  final String latitude;
+  final String listedBy;
+  final String longitude;
+  final String lotSize;
+  final String lotSize2;
+  final String propertyFloor;
+  final String propertyType;
+  final String startingBid;
+  final String subtitle2FC;
+  final String subtitle3FC;
+  final String subtitle4FC;
+  final String subtitleFC;
+  final String title;
+  final String title4FC;
+  final String totalFloor;
+  final String trailing2FC;
+  final String trailing3FC;
+  final String trailing4FC;
+  final String trailingFC;
+  final String type;
+
+  Property({
+    required this.acceptBuyersTitle,
+    required this.architextName,
+    required this.assetType,
+    required this.builderName,
+    required this.builtupArea,
+    required this.carParking,
+    required this.interiorAccess,
+    required this.ownership,
+    required this.possesionTime,
+    required this.propertyBuiltYear,
+    required this.propertyTaxID,
+    required this.rentable,
+    required this.address,
+    required this.auctionStatus,
+    required this.builtUpArea,
+    required this.category,
+    required this.currentBid,
+    required this.description,
+    required this.id,
+    required this.imageAssetPath,
+    required this.imagePaths,
+    required this.index,
+    required this.latitude,
+    required this.listedBy,
+    required this.longitude,
+    required this.lotSize,
+    required this.lotSize2,
+    required this.propertyFloor,
+    required this.propertyType,
+    required this.startingBid,
+    required this.subtitle2FC,
+    required this.subtitle3FC,
+    required this.subtitle4FC,
+    required this.subtitleFC,
+    required this.title,
+    required this.title4FC,
+    required this.totalFloor,
+    required this.trailing2FC,
+    required this.trailing3FC,
+    required this.trailing4FC,
+    required this.trailingFC,
+    required this.type,
+  });
+
+  factory Property.fromJson(Map<String, dynamic> json) {
+    return Property(
+      acceptBuyersTitle: json['AcceptBuyersTitle'],
+      architextName: json['ArchitextName'],
+      assetType: json['AssetType'],
+      builderName: json['BuilderName'],
+      builtupArea: json['BuiltupArea'],
+      carParking: json['CarParking'],
+      interiorAccess: json['InteriorAccess'],
+      ownership: json['Ownership'],
+      possesionTime: json['PossesionTime'],
+      propertyBuiltYear: json['PropertyBuiltYear'],
+      propertyTaxID: json['PropertyTaxID'],
+      rentable: json['Rentable'],
+      address: json['address'],
+      auctionStatus: json['auctionStatus'],
+      builtUpArea: json['builtUpArea'],
+      category: json['category'],
+      currentBid: json['currentBid'],
+      description: json['description'],
+      id: json['id'],
+      imageAssetPath: json['imageAssetPath'],
+      imagePaths: List<String>.from(json['imagePaths']),
+      index: json['index'],
+      latitude: json['latitude'],
+      listedBy: json['listedBy'],
+      longitude: json['longitude'],
+      lotSize: json['lotSize'],
+      lotSize2: json['lotSize2'],
+      propertyFloor: json['propertyFloor'],
+      propertyType: json['propertyType'],
+      startingBid: json['startingBid'],
+      subtitle2FC: json['subtitle2FC'],
+      subtitle3FC: json['subtitle3FC'],
+      subtitle4FC: json['subtitle4FC'],
+      subtitleFC: json['subtitleFC'],
+      title: json['title'],
+      title4FC: json['title4FC'],
+      totalFloor: json['totalFloor'],
+      trailing2FC: json['trailing2FC'],
+      trailing3FC: json['trailing3FC'],
+      trailing4FC: json['trailing4FC'],
+      trailingFC: json['trailingFC'],
+      type: json['type'],
+    );
+  }
+}
--- lib/screens/widgets/bottom_navigation_homescreen/profile_screen_cells/past_auction.dart
+++ lib/screens/widgets/bottom_navigation_homescreen/profile_screen_cells/past_auction.dart
@@ -1,143 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-class Property {
-  final String acceptBuyersTitle;
-  final String architextName;
-  final String assetType;
-  final String builderName;
-  final String builtupArea;
-  final String carParking;
-  final String interiorAccess;
-  final String ownership;
-  final String possesionTime;
-  final String propertyBuiltYear;
-  final String propertyTaxID;
-  final String rentable;
-  final String address;
-  final String auctionStatus;
-  final String builtUpArea;
-  final String category;
-  final String currentBid;
-  final String description;
-  final String id;
-  final String imageAssetPath;
-  final List<String> imagePaths;
-  final int index;
-  final String latitude;
-  final String listedBy;
-  final String longitude;
-  final String lotSize;
-  final String lotSize2;
-  final String propertyFloor;
-  final String propertyType;
-  final String startingBid;
-  final String subtitle2FC;
-  final String subtitle3FC;
-  final String subtitle4FC;
-  final String subtitleFC;
-  final String title;
-  final String title4FC;
-  final String totalFloor;
-  final String trailing2FC;
-  final String trailing3FC;
-  final String trailing4FC;
-  final String trailingFC;
-  final String type;
-
-  Property({
-    required this.acceptBuyersTitle,
-    required this.architextName,
-    required this.assetType,
-    required this.builderName,
-    required this.builtupArea,
-    required this.carParking,
-    required this.interiorAccess,
-    required this.ownership,
-    required this.possesionTime,
-    required this.propertyBuiltYear,
-    required this.propertyTaxID,
-    required this.rentable,
-    required this.address,
-    required this.auctionStatus,
-    required this.builtUpArea,
-    required this.category,
-    required this.currentBid,
-    required this.description,
-    required this.id,
-    required this.imageAssetPath,
-    required this.imagePaths,
-    required this.index,
-    required this.latitude,
-    required this.listedBy,
-    required this.longitude,
-    required this.lotSize,
-    required this.lotSize2,
-    required this.propertyFloor,
-    required this.propertyType,
-    required this.startingBid,
-    required this.subtitle2FC,
-    required this.subtitle3FC,
-    required this.subtitle4FC,
-    required this.subtitleFC,
-    required this.title,
-    required this.title4FC,
-    required this.totalFloor,
-    required this.trailing2FC,
-    required this.trailing3FC,
-    required this.trailing4FC,
-    required this.trailingFC,
-    required this.type,
-  });
-
-  factory Property.fromJson(Map<String, dynamic> json) {
-    return Property(
-      acceptBuyersTitle: json['AcceptBuyersTitle'],
-      architextName: json['ArchitextName'],
-      assetType: json['AssetType'],
-      builderName: json['BuilderName'],
-      builtupArea: json['BuiltupArea'],
-      carParking: json['CarParking'],
-      interiorAccess: json['InteriorAccess'],
-      ownership: json['Ownership'],
-      possesionTime: json['PossesionTime'],
-      propertyBuiltYear: json['PropertyBuiltYear'],
-      propertyTaxID: json['PropertyTaxID'],
-      rentable: json['Rentable'],
-      address: json['address'],
-      auctionStatus: json['auctionStatus'],
-      builtUpArea: json['builtUpArea'],
-      category: json['category'],
-      currentBid: json['currentBid'],
-      description: json['description'],
-      id: json['id'],
-      imageAssetPath: json['imageAssetPath'],
-      imagePaths: List<String>.from(json['imagePaths']),
-      index: json['index'],
-      latitude: json['latitude'],
-      listedBy: json['listedBy'],
-      longitude: json['longitude'],
-      lotSize: json['lotSize'],
-      lotSize2: json['lotSize2'],
-      propertyFloor: json['propertyFloor'],
-      propertyType: json['propertyType'],
-      startingBid: json['startingBid'],
-      subtitle2FC: json['subtitle2FC'],
-      subtitle3FC: json['subtitle3FC'],
-      subtitle4FC: json['subtitle4FC'],
-      subtitleFC: json['subtitleFC'],
-      title: json['title'],
-      title4FC: json['title4FC'],
-      totalFloor: json['totalFloor'],
-      trailing2FC: json['trailing2FC'],
-      trailing3FC: json['trailing3FC'],
-      trailing4FC: json['trailing4FC'],
-      trailingFC: json['trailingFC'],
-      type: json['type'],
-    );
-  }
-}
+import '../../../../model/property_model.dart';
+import '../../../propertyDetail_screen.dart';
+import '../../propertyCard.dart';
 
 Future<List<Property>> fetchProperties() async {
   final response = await http.get(Uri.parse('https://api-9bea.onrender.com/properties/'));
@@ -158,47 +24,78 @@ class PastAuction extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Past Auctions'),
+        title: const Text('Past Auctions', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+        centerTitle: true,
       ),
       body: FutureBuilder<List<Property>>(
         future: fetchProperties(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('Failed to load properties'));
+            return const Center(child: Text('Failed to load properties'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No properties available'));
+            return const Center(child: Text('No properties available'));
           } else {
             List<Property> properties = snapshot.data!;
-            return ListView.builder(
-              itemCount: properties.length,
-              itemBuilder: (context, index) {
-                Property property = properties[index];
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.asset(property.imageAssetPath),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Text(
-                            property.title,
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            // Filter properties with index greater than 5
+            List<Property> filteredProperties = properties.where((property) => property.index > 5).toList();
+
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 0, 16, 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 10),
+                    ...filteredProperties.map((property) {
+                      return Column(
+                        children: [
+                          PropertyCard(
+                            imageAssetPath: property.imageAssetPath,
+                            type: property.type,
+                            title: property.title,
+                            address: property.address,
+                            builtUpArea: property.builtUpArea,
+                            lotSize: property.lotSize,
+                            auctionStatus: property.auctionStatus,
+                            currentBid: property.currentBid,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PropertyDetailScreen(
+                                    title: property.title,
+                                    id: property.id,
+                                    address: property.address,
+                                    imagePaths: property.imagePaths,
+                                    auctionStatus: property.auctionStatus,
+                                    latitude: property.latitude,
+                                    longitude: property.longitude,
+                                    description: property.description,
+                                    propertyIndex: property.index,
+                                    title4: property.title4FC,
+                                    subtitleFC: property.subtitleFC,
+                                    subtitle2FC: property.subtitle2FC,
+                                    subtitle3FC: property.subtitle3FC,
+                                    subtitle4FC: property.subtitle4FC,
+                                    trailingFC: property.trailingFC,
+                                    trailing2FC: property.trailing2FC,
+                                    trailing3FC: property.trailing3FC,
+                                    trailing4FC: property.trailing4FC,
+                                  ),
+                                ),
+                              );
+                            },
                           ),
-                        ),
-                        Text('Address: ${property.address}\n'),
-                        Text('Description: ${property.description}\n'),
-                        Text('Type: ${property.type}'),
-                        Text('Built-up Area: ${property.builtUpArea}'),
-                        Text('Lot Size: ${property.lotSize}'),
-                      ],
-                    ),
-                  ),
-                );
-              },
+                          const SizedBox(height: 10),
+                        ],
+                      );
+                    }).toList(),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
             );
           }
         },
